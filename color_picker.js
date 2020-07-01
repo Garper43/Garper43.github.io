a2e3854ec45575dfbd24b3a98b47cd9b9131e578
Garper43 <65208305+Garper43@users.noreply.github.com>
2020-07-01 16:34:57 -0700

    Add files via upload

---
+++ color_picker.js
@@ -0,0 +1,48 @@
+var picker = document.getElementById('color_picker');
+var sliders = document.getElementById('sliders');
+var preview = document.getElementById('color_picker_preview');
+var slider = {
+	r: document.getElementsByClassName('rgb_slider')[0],
+	g: document.getElementsByClassName('rgb_slider')[1],
+	b: document.getElementsByClassName('rgb_slider')[2],
+}
+var drag = false;
+var rgb
+
+sliders.addEventListener( 'mousemove', function color(ev) {
+	var rgb = 'rgb(' + slider.r.value + ',' + slider.g.value + ',' + slider.b.value + ')';
+	preview.style.backgroundColor = rgb;
+} )
+
+	var rgb = 'rgb(' + slider.r.value + ',' + slider.g.value + ',' + slider.b.value + ')';
+	preview.style.backgroundColor = rgb;
+
+function find( e , a ) { 
+	return Number(eval('e.style.' + a).slice( 0 , eval('e.style.' + a).indexOf('px') ));
+}
+
+function set( e , x , y ) {
+	e.style.left = x + 'px';
+	e.style.top = y + 'px';
+}
+
+var Sx;
+var Sy;
+
+preview.addEventListener( 'mousedown' , function(ev) {
+	drag = true;
+	x = ev.clientX;
+	y = ev.clientY;
+	Sx = x - find( picker, 'left' );
+	Sy = y - find( picker, 'top' );
+} )
+document.addEventListener( 'mouseup' , function() {
+	drag = false;
+} )
+preview.addEventListener( 'mousemove' , function(ev) {
+	if( drag ) {
+		x = ev.clientX;
+		y = ev.clientY;
+		set( picker , x - Sx , y - Sy )
+	}
+} )
